48a49f77ea1439448ca4e69ad4907be602c173b7
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-25 14:22:01 -0300

    feat: Criado folder Entities com classes Jogador, PerfilCompetitivo e Time, com 1:N Time > Jogadores e 1:1 Jogador > PerfilCompetitivo

---
+++ CP3/Entities/Jogador.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CP3.Entities;
+
+public class Jogador
+{
+    [Key]
+    [MaxLength(80)]
+    public string Nickname { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(50)]
+    public string Funcao { get; set; } = string.Empty;
+    
+    [Range(12, 60)]
+    public int Idade  { get; set; }
+    
+    [ForeignKey("Time")]
+    public int TimeId { get; set; }
+    
+    public Time? Time { get; set; }
+    
+    public PerfilCompetitivo?  PerfilCompetitivo { get; set; }
+}
